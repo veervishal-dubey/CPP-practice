4f8e6bc141bbda7b59422d4d3d171b5521031df0
Veer Vishal Dubey <veervishaldubey@gmail.com>
2025-11-27 09:31:19 +0530

    Introduce arrptr for array pointer demonstration
    Added arrptr to demonstrate array pointer access alongside ptr.

--- array pointers.cpp
+++ array pointers.cpp
@@ -4,8 +4,9 @@ using namespace std;
 int main()
 {
     int v[3]={120,43,61};
-    int *ptr;
+    int *ptr,*arrptr;
     ptr=v;
+    arrptr=&v[0];
     cout<<"v is "<<&v<<"\n\n";
     
     for (int i=0;i<3;i++)
@@ -14,6 +15,11 @@ int main()
         cout<<"value at *ptr "<<*ptr<<"\n";
         cout<<"value at &ptr"<<&ptr<<"\n";
         ptr++;
+        cout<<"value at arrptr "<<(arrptr+i)<<"\n";
+        cout<<"value at *arrptr "<<*(arrptr+i)<<"\n";
+        cout<<"value at &arrptr "<<&arrptr<<"\n";
+        
+        cout<<"\n\n";
     }
     
     int *ptr1,*ptr2,*ptr3;
@@ -24,7 +30,7 @@ int main()
     // this gives the difference of indices. This has a meaningful result in the memory because of the difference of indices.
     cout<<*ptr1-*ptr2<<"\n";
     // this gives the difference of the values.
-    cout<<ptr1+ptr3<<'\n';
+    // cout<<ptr1+ptr3<<'\n';
     // this will not work. This does not have a meaningful result in memory.
     cout<<*ptr1+*ptr3<<"\n";
     return 0;
