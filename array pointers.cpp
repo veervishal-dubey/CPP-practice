8d49f1673aced023213d076890e10b889789ea0f
Veer Vishal Dubey <veervishaldubey@gmail.com>
2025-11-27 09:26:30 +0530

    Enhance pointer examples in array pointers.cpp
    Added pointer arithmetic examples to demonstrate index differences and value differences.

--- array pointers.cpp
+++ array pointers.cpp
@@ -15,6 +15,17 @@ int main()
         cout<<"value at &ptr"<<&ptr<<"\n";
         ptr++;
     }
-
+    
+    int *ptr1,*ptr2,*ptr3;
+    ptr1=&v[0];
+    ptr2=&v[2];
+    ptr3=&v[1];
+    cout<<ptr1-ptr2<<"\n"; 
+    // this gives the difference of indices. This has a meaningful result in the memory because of the difference of indices.
+    cout<<*ptr1-*ptr2<<"\n";
+    // this gives the difference of the values.
+    cout<<ptr1+ptr3<<'\n';
+    // this will not work. This does not have a meaningful result in memory.
+    cout<<*ptr1+*ptr3<<"\n";
     return 0;
 }
